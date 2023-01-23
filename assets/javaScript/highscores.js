96ca2481cbe9de16f6c5f542ee0fb64fa74b4d7a
Wendy <118736912+WendyVsQz@users.noreply.github.com>
2023-01-23 19:34:23 +0000

    code updates

--- assets/javaScript/highscores.js
+++ assets/javaScript/highscores.js
@@ -1,17 +1,98 @@
 
-// 5. score display
+var highScores=[] // initialization
+getHighScores(); // retrieve high score list from local storage
+setAnswerButtons(); // add event listeners to all answer buttons
 
-const letter = document.getElementById("Print");
-let score =  0;
-function scores() {
-    score += 100;
-    letter.innerHTML = scores;
+// Add event listeners to the rest of the buttons
+document.getElementById("highScoresBtn").addEventListener("click", displayHighScores);
+document.getElementById("goBackBtn").addEventListener("click", goBack);
+document.getElementById("clearScores").addEventListener("click", clearHighscores);
+document.getElementById("initialsBtn").addEventListener("click", saveInitials);
+
+// Onclick event for "View Highscores" button on navigation bar
+function displayHighScores() {
+    var highscoresSection = document.getElementById("highscores");
+    var highscoresList = document.getElementById("highscoreList");
+
+    clearInterval(interval); // Just in case timer is running
+
+    // Delete current list items
+    clearHighscoresList();
+
+    // Create list items for each high scorer
+    for (var i=0; i < highScores.length; i++) {
+        var newListItem = document.createElement("li");
+        newListItem.textContent = highScores[i];
+        highscoresList.appendChild(newListItem);
+    }
+
+    // Hide all sections, including navigation bar, then unhide highscores section
+    hideAll();
+    document.querySelector("nav").hidden = true;
+    highscoresSection.hidden = false;
 }
 
+// Onclick event for "Go Back" button on Highscores section
+function goBack() {
+    // Reset timer
+    timerVal = 75;
+    document.getElementById("timerValue").textContent = timerVal.toString();
 
-//6. save initial and score
+    // Hide all sections, then unhide navigation bar and welcome section
+    hideAll();
+    document.querySelector("nav").hidden = false;
+    document.getElementById("welcome").hidden = false;
+}
+// Retrieves highscore list from localstorage, if it's there
+function getHighScores() {
+    var hsList = JSON.parse(localStorage.getItem(hsStorage));
 
+    if (hsList) {
+        highScores = hsList;
+    }
+}
 
+// Stores highscore list to localstorage
+function setHighScores() {
+    localStorage.setItem(hsStorage, JSON.stringify(highScores));
+}
+// Onclick event for "Submit" button on Complete section
+function saveInitials() {
+    var initialsInput = document.getElementById("initials"); // get Input element
+    var score = 0; // for retrieving scores from highScores list
+    var newIndex = 0; // index of new high score
 
+    // Determine where in the list the new initials belong
+    for (newIndex=0; newIndex < highScores.length; newIndex++) {
+        // get just the score out of the string
+        score = parseInt(highScores[newIndex].substr(-2));
+        // If current score (timerVal) is greater than the indexed score, we've found where it goes
+        if (timerVal > score) {
+            break;
+            }
+        }
+    }
 
-// 7. ability to clear history
+// Removes list items from Highscores section
+function clearHighscoresList() {
+    var highscoresList = document.getElementById("highscoreList");
+
+    // Delete each list item until none remain
+    while (highscoresList.childNodes.length > 0) {
+        highscoresList.removeChild(highscoresList.childNodes[0]);
+    }
+}
+
+// Onclick event for "Clear Highscores" button on Highscores section
+function clearHighscores() {
+    // Delete each list item form Highscores section
+    clearHighscoresList();
+    
+    // Clear highscores list and write to localstorage
+    highScores = [];
+    setHighScores();
+}
+highScores.splice(newIndex, 0, initialsInput.value + " - " + timerVal.toString()); // add high score to beginning
+    initialsInput.value = ""; // clear Input element
+    setHighScores(); // save new highscores list to localstorage
+    displayHighScores(); // display the highscores
--- assets/javaScript/questions.js
+++ assets/javaScript/questions.js
@@ -1,105 +1,131 @@
 
 // 1. generateQuiz function
+const showQuestions = document.querySelector('#question');
+const choices = Array.from(document.querySelector('#.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
 
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+var currentQuestion = {}
+var acceptAnswers = true;
+var score = 0;
+var questionCounter = 0
+var availableQuestions = []
+
+// Add event listeners to the rest of the buttons
+document.getElementById("startBtn").addEventListener("click", startQuiz);
+
 
 // 2. display question and answers
-    function questions (questions, quizContainer){
+
 
         var questions = [
             {
-                prompt:"Which planet has the most moons?\n(a) saturn \n\(b) venus \n(c)jupiter",
-                answer:"Saturn"
+                prompt:"Which planet has the most moons?,choice1:(a) saturn, choice2:(b), venus, choice3:(c)jupiter",
+                answer: "Saturn",
             },
         
             {
-                prompt:"What part of a plant conducts photosynthesis?\n(a) root \n\(b) leaf \n(c)stalk",
-                answer:"leaf"
+                prompt:"What part of a plant conducts photosynthesis?, choice1:(a) root, choice2:(b) leaf, choice3:(c)stalk",
+                answer:"leaf",
             },
         
             {
-                prompt:"How many elements are in the periodic table?\n(a) 100 \n\(b) 97 \n(c)118",
-                answer:"118"
+                prompt:"How many elements are in the periodic table?choice1:(a) 100, choice2:(b) 97, choice3:(c)118",
+                answer: 118,
             },
         
             {
-                prompt:"Where is the smallest bone in the human body located?\n(a) phalanges \n\(b) femur  \n(c)stapes",
+                prompt:"Where is the smallest bone in the human body located?choice1:(a) phalanges, choice2:(b) femur, choice3:(c)stapes",
                 answer:"stapes"
             },
         
             {
-                prompt:"How many hearts does an octopus have?\n(a) 5 \n\(b) 3 \n(c)1",
-                answer:"3"
+                prompt:"How many hearts does an octopus have?choice1:(a) 5, choice2:(b) 3, choice3:(c)1",
+                answer: 3,
             },
         ];
-    }
-}
-    function showQuestions(question, quizContainer){
-    
-    var output = [];
-    var choices;
-    // for each question...
-    for(var i=0; i<questions.length; i++) {
-        // reset the list of answers
 
-        choices= [];
+        
 
-        // for each available answer to this question..
-    }
-    for(letter in questions[i].answers){
+       // Onclick event for "Start Quiz" button on welcome section
+function startQuiz() {
+    // Hide all sections, then unhide the first page
+    hideAll();
+    document.getElementById("page1").hidden = false;
 
-        choices.push(
-            '<label>'
-            +'<label>'
-            + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-            + letter + ': '
-            + questions[i].answers[letter]
-        + '</label>'
-    );
-    
+    // Start timer:
+    timerVal = 76; // start with 76 seconds - will be decremented before display
+    clearInterval(interval); // make sure no timer is already running
+    //Call updateTimer once every second
+    interval = setInterval(updateTimer, 1000);
 }
 
-// add this question and its answers to the output
-output.push(
-    '<div class="question">' + questions[i].question + '</div>'
-    + '<div class="answers">' + choices.join('') + '</div>'
-);
+        // Onclick event for all answer buttons
+function answerButtons(event) {
+    var currentPage = event.target.parentElement.getAttribute("name"); // index of current page
+    var nextPage = parseInt(currentPage) + 1; // index of next page
+    var correctAnswer = event.target.parentElement.getAttribute("value"); // index of correct answer
+    var userAnswer = event.target.value; // index of user's answer
+
+    // If time is up, go to last page; can be less than 0 because 10 subtracted for wrong answers
+    if (timerVal <= 0) {
+        nextPage = lastPage;
+    }
+    // Advance to next page
+    goToPage(nextPage);
+
+    // If user entered correct answer, then display "Correct"
+    if (userAnswer === correctAnswer) {
+        document.getElementById("correct").hidden = false;
+    }
+    // If user entered incorrect answer, then display "Incorrect", and take away 10 seconds from timer
+    else {
+        document.getElementById("incorrect").hidden = false;
+        timerVal = timerVal - 10;
+    }
+}
+// Adds onclick event to all 20 answer buttons
+function setAnswerButtons() {
+    var answerBtns = document.getElementsByClassName("answerBtn")
+    for (var i = 0; i < answerBtns.length; i++) {
+        answerBtns[i].addEventListener("click", answerButtons);
+    }
 }
 
-// finally combine our output list into one string of html and put it on the page
-quizContainer.innerHTML = output.join('');
+// Hides all sections; used before displaying current section
+function hideAll() {
+    var sections = document.getElementsByTagName("section");
 
-showQuestions(questions, quizContainer);
-
-// 3. Show the results
-
-function showResults(questions, quizContainer, resultsContainer){
-
-// gather answer containers from our quiz
-var answerContainers = quizContainer.querySelectorAll('.answers');
-	
-// keep track of user's answers
-var userAnswer = '';
-var numCorrect = 0;
-
-// for each question..
-for(var i=0; i < questions.length; i++) {
-
-// find selected answer..
-
-    var answer = window.prompt(questions[i].prompt);
-
-    if(answer === questions[i].choices){
-    score++;
-    alert("correct");
-    } else {
-    alert("wrong");
-        }
-    
+    for (var i=0; i < sections.length; i++) {
+        sections[i].hidden = true;
     }
-};
+}
 // when user clicks submit, show results
 submitButton.onclick = function(){
     showResults(questions, quizContainer, resultsContainer);
+    }
+function updateTimer() {
+    // Check for <0 just in case - can happen because 10 seconds subtracted for wrong answers!
+    if (timerVal <= 0) {
+        goToPage(lastPage);
+        alert("Time's up!");
+    }
+    else {
+        timerVal--; // decrement timer
+        document.getElementById("timerValue").textContent = timerVal.toString(); // display current timer value
+    }
 }
 
+function goToPage(nextPg) {
+    // If quiz is done, stop timer, update score on Complete section
+    if (nextPg === lastPage) {
+        clearInterval(interval);
+        document.getElementById("finalScore").textContent = timerVal.toString();
+    }
+
+    // Hide everything, then determine which sections to unhide
+    hideAll();
+
+    // Advance to next page
+    document.getElementsByName(nextPg.toString())[0].hidden = false;
+}
